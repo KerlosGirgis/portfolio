2813bd5c611ad56bc75c936c40f14e84574d9ab6
KerlosGirgis <kerlos0@protonmail.com>
2026-05-28 15:00:30 +0300

    Update project dependencies, enhance project card layout, and add new screenshots

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,6 +1,11 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "../projectCard/projectCard";
 import appImg1 from "../../assets/img/todo.png";
+import appImg11 from "../../assets/img/todo1.png";
+import appImg12 from "../../assets/img/todo2.png";
+import appImg13 from "../../assets/img/todo3.png";
+import appImg14 from "../../assets/img/todo4.png";
+import appImg15 from "../../assets/img/todo5.png";
 import appImg2 from "../../assets/img/alpha.png";
 import appImg3 from "../../assets/img/weather.png";
 import appImg4 from "../../assets/img/shoppy.png";
@@ -9,13 +14,24 @@ import websiteImg2 from "../../assets/img/store.png"
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
+type ProjectData = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  screenshots: string[];
+  githubUrl?: string;
+  fdroidUrl?: string;
+  websiteUrl?: string;
+};
+
 export const Projects = () => {
-  const apps = [
+  const apps: ProjectData[] = [
     {
       title: "Todo",
       description:
         "Offline, secure, To-Do and Notes app with encrypted storage",
       imgUrl: appImg1,
+      screenshots: [appImg11, appImg12, appImg13, appImg14, appImg15],
       githubUrl: "https://github.com/KerlosGirgis/Todo",
       fdroidUrl: "https://f-droid.org/en/packages/com.k.todo/"
     },
@@ -23,18 +39,21 @@ export const Projects = () => {
       title: "Alpha",
       description: "Simple Bookstore Application",
       imgUrl: appImg2,
+      screenshots: [appImg2],
       githubUrl: "https://github.com/KerlosGirgis/alpha"
     },
     {
       title: "Weather",
       description: "Simple Weather Forcast App",
       imgUrl: appImg3,
+      screenshots: [appImg3],
       githubUrl: "https://github.com/KerlosGirgis/Weather"
     },
-        {
+    {
       title: "Shoppy",
       description: "Flutter-based e-commerce app",
       imgUrl: appImg4,
+      screenshots: [appImg4],
       githubUrl: "https://github.com/KerlosGirgis/Shoppy"
     },
   ];
@@ -46,6 +65,7 @@ export const Projects = () => {
       description:
         "Movibes is a sleek and modern movie browsing website",
       imgUrl: websiteImg1,
+      screenshots: [websiteImg1],
       githubUrl: "https://github.com/KerlosGirgis/Movibes",
       websiteUrl: "https://kerlosgirgis.github.io/Movibes/movies"
     },
@@ -54,10 +74,30 @@ export const Projects = () => {
       description:
         "E-commerce website with modern UI",
       imgUrl: websiteImg2,
+      screenshots: [websiteImg2],
       githubUrl: "https://github.com/KerlosGirgis/Store",
       websiteUrl: "https://kerlosgirgis.github.io/store/"
     },
   ];
+
+  const renderProjectRows = (projects: ProjectData[]) => {
+    return projects.map((project, index) => (
+      <div key={index} className="project-row">
+        <div className="project-card-wrapper">
+          <ProjectCard {...project} />
+        </div>
+        <div className="project-screenshots">
+          <div className="screenshot-track">
+            {project.screenshots?.map((screenshot: string, screenshotIndex: number) => (
+              <div key={screenshotIndex} className="screenshot-item">
+                <img src={screenshot} alt={`${project.title} screenshot ${screenshotIndex + 1}`} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    ));
+  };
 
   return (
     <section className="project" id="projects">
@@ -82,26 +122,12 @@ export const Projects = () => {
                         <Nav.Link eventKey="second">Websites</Nav.Link>
                       </Nav.Item>
                     </Nav>
-                    <Tab.Content
-                      id="slideInUp"
-                    >
+                    <Tab.Content id="slideInUp">
                       <Tab.Pane eventKey="first">
-                        <Row>
-                          {apps.map((apps, index) => {
-                            return <ProjectCard key={index} {...apps} />;
-                          })}
-                        </Row>
+                        <div className="project-list">{renderProjectRows(apps)}</div>
                       </Tab.Pane>
-                    </Tab.Content>
-                    <Tab.Content
-                      id="slideInUp"
-                    >
                       <Tab.Pane eventKey="second">
-                        <Row>
-                          {websites.map((websites, index) => {
-                            return <ProjectCard key={index} {...websites} />;
-                          })}
-                        </Row>
+                        <div className="project-list">{renderProjectRows(websites)}</div>
                       </Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
